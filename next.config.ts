bff01298d43c3040abc7ef3159b8de80eb9edc4e
Alper Yildirim <kemalper@yahoo.com>
2026-07-23 13:34:37 +0300

    Rewrite dbs.surgicheck.net to DBS pre-assessment page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [{ type: "host", value: "dbs.surgicheck.net" }],
+        destination: "/treatments/dbs-assessment",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
